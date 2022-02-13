b4dd218e092c11db284b84d6647e14a47e499907
JLL32 <eabduljalil@outlook.com>
2022-02-13 23:42:33 +0100

    rename str to string

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -4,15 +4,15 @@
 #include <iostream>
 
 int main() {
-  std::string str = "HI THIS IS BRAIN";
-  std::string *stringPTR = &str;
-  std::string &stringREF = str;
+  std::string string = "HI THIS IS BRAIN";
+  std::string *stringPTR = &string;
+  std::string &stringREF = string;
 
-  std::cout << "address of string        : " << &str << std::endl;
+  std::cout << "address of string        : " << &string << std::endl;
   std::cout << "address held by stringPTR: " << stringPTR << std::endl;
   std::cout << "address held by stringREF: " << &stringREF << std::endl;
   std::cout << std::endl;
-  std::cout << "value of string           : " << str << std::endl;
+  std::cout << "value of string           : " << string << std::endl;
   std::cout << "value pointed by stringPTR: " << *stringPTR << std::endl;
   std::cout << "value pointed by stringREF: " << stringREF << std::endl;
 }
