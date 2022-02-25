738c1a189d58a95ff893cbb7cffe44e95d5f43ac
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-02-25 18:59:48 +0100

    add module03/ex00

---
+++ module03/ex00/ClapTrap.hpp
@@ -0,0 +1,36 @@
+//
+// Created by jll32 on 25/2/2022.
+//
+
+#ifndef EX00_CLAPTRAP_HPP
+#define EX00_CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap {
+public:
+    ClapTrap();
+
+    ClapTrap(const ClapTrap &copy);
+
+    explicit ClapTrap(const std::string &name);
+
+    ~ClapTrap();
+
+    ClapTrap &operator=(const ClapTrap &operand);
+
+    void attack(const std::string &target);
+
+    void takeDamage(unsigned int amount);
+
+    void beRepaired(unsigned int amount);
+
+private:
+    std::string _name;
+    unsigned int _hit_points;
+    unsigned int _energy_points;
+    unsigned int _attack_damage;
+};
+
+
+#endif //EX00_CLAPTRAP_HPP
---
+++ module03/ex00/main.cpp
@@ -0,0 +1,9 @@
+#include "ClapTrap.hpp"
+
+int main() {
+    ClapTrap l3arbi("L3arbi");
+    ClapTrap dawya("Dawya");
+    l3arbi.attack("Dawya");
+    dawya.beRepaired(10);
+    return 0;
+}
