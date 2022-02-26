e45cd6646a0642e131258a07f3da6b7340ef4f56
JLL32 <eabduljalil@outlook.com>
2022-02-26 19:47:34 +0100

    Add module05/ex00

---
+++ module05/ex00/Bureaucrat.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Abdeljalil El hachimi on 2/26/22.
+//
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _NAME("") {
+    std::cout << "Bureaucrat Default Constructor" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string &name,
+                       int grade) throw(Bureaucrat::GradeTooHighException, Bureaucrat::GradeTooLowException): _NAME(
+        name) {
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    std::cout << "Bureaucrat Name, Grade Constructor" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy) {
+    *this = copy;
+    std::cout << "Bureaucrat Copy Constructor" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat() {
+    std::cout << "Bureaucrat Destructor" << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs) {
+    this->_grade = rhs._grade;
+    return *this;
+}
+
+std::string Bureaucrat::getName() const {
+    return this->_NAME;
+}
+
+int Bureaucrat::getGrade() const {
+    return this->_grade;
+}
+
+void Bureaucrat::incrementGrade() throw(Bureaucrat::GradeTooHighException) {
+    if (_grade == 1)
+        throw Bureaucrat::GradeTooHighException();
+    this->_grade--;
+}
+
+void Bureaucrat::decrementGrade() throw(Bureaucrat::GradeTooLowException) {
+    if (_grade == 150)
+        throw Bureaucrat::GradeTooLowException();
+    this->_grade++;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+    return "Oops! too high for grade";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+    return "Oops! too low for grade";
+}
---
+++ module05/ex00/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Bureaucrat.hpp"
+
+int main() {
+    Bureaucrat bob("Bob", 150);
+
+    try {
+        bob.decrementGrade();
+    } catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+
+    }
+
+  return 0;
+}
