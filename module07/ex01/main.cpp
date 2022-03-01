34da7a7ce16c4913498d218cdba3ae4b04cd880f
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-03-02 00:56:28 +0100

    more tests for module07/ex01

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -2,7 +2,12 @@
 #include "iter.hpp"
 
 int main() {
+
     const char *str = "hello world";
+    int arr[] = {1, 2, 3, 4, 5};
+
     iter<const char>(str, strlen(str), printElement);
+    iter<const int>(arr, 5, printElement);
+
     return 0;
 }
