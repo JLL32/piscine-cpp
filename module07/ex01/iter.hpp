84adacb7d4cf43ae95b334d7f376df077e518700
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-03-02 01:58:53 +0100

    better iter

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -8,14 +8,14 @@
 #include <iostream>
 
 template <typename T>
-void iter(const T *array, size_t size, void (*f)(T &)) {
+void iter(const T *array, size_t size, void (*f)(const T &)) {
     for (size_t i = 0; i < size; i++) {
         f(array[i]);
     }
 }
 
 template <typename T>
-void printElement(T &elem) {
+void printElement(const T &elem) {
     std::cout << elem << std::endl;
 }
 
