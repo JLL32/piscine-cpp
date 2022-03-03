9997d5b85d4f1c3d9142101d761087596d771906
JLL32 <eabduljalil@outlook.com>
2022-03-03 14:57:55 +0100

    fix typo

--- module06/ex01/main.cpp
+++ module06/ex01/main.cpp
@@ -21,14 +21,14 @@ int main() {
     Data *data = new Data();
     uintptr_t raw = serialize(data);
 
-    std::cout << "data->hight: " << data->height << std::endl;
+    std::cout << "data->height: " << data->height << std::endl;
     std::cout << "data->age: " << data->age << std::endl;
 
     std::cout << "raw[0]: " << reinterpret_cast<int *>(raw)[0] << std::endl;
     std::cout << "raw[1]: " << reinterpret_cast<int *>(raw)[1] << std::endl;
 
     Data *data2 = deserialize(raw);
-    std::cout << "data2->hight: " << data2->height << std::endl;
+    std::cout << "data2->height: " << data2->height << std::endl;
     std::cout << "data2->age: " << data2->age << std::endl;
 
     delete data;
