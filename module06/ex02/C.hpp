56933c08d58a40866f05d769b7459a42370d5a10
JLL32 <eabduljalil@outlook.com>
2022-03-03 15:02:34 +0100

    Add C.hpp

---
+++ module06/ex02/C.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Abdeljalil El hachimi on 3/3/22.
+//
+
+#ifndef MODULE06_C_HPP
+#define MODULE06_C_HPP
+
+#include "Base.hpp"
+
+class C: public Base {
+};
+
+
+#endif //MODULE06_C_HPP
